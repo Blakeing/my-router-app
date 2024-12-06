d8759092789eda900f9b8f8136d685b8ebe479b9
Blake Ingenthron <blakeingenthron@gmail.com>
2024-12-05 22:21:01 -0600

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import { fileURLToPath } from "url";
+
+export default tseslint.config({
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.json"],
+      tsconfigRootDir: fileURLToPath(new URL(".", import.meta.url)),
+    },
+  },
+  settings: {
+    react: {
+      version: "19.0",
+    },
+  },
+  plugins: {
+    react,
+  },
+  rules: {
+    ...tseslint.configs.recommendedTypeChecked.rules,
+    ...react.configs.recommended.rules,
+    ...react.configs["jsx-runtime"].rules,
+  },
+});
